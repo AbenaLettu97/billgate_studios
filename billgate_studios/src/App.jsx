199b9e58cd1c5e547eeaf823b840c6908a39fbfe
EuniceAbenaLettu <abenalettu@gmail.com>
2025-06-30 14:46:48 +0000

    installed framer motion

--- billgate_studios/src/App.jsx
+++ billgate_studios/src/App.jsx
@@ -1,11 +1,12 @@
+import React from 'react'
 
 
 function App() {
 
   return (
-    <>
-     
-    </>
+    <React.Fragment>
+     <h1 className='bg-blue-950 text-white'>This is Home</h1>
+    </React.Fragment>
   )
 }
 
