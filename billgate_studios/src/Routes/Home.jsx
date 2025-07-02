b676876915fe3f8107b476fd64280a6ba51d206e
EuniceAbenaLettu <abenalettu@gmail.com>
2025-07-02 15:05:24 +0000

    navbar styling

--- billgate_studios/src/Routes/Home.jsx
+++ billgate_studios/src/Routes/Home.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-   <>
-      <div>
-        <h1 className='text-8xl items-center justify-center h-full'>Capture the Moment</h1>
-      </div>
-   </>
+    <div>Home</div>
   )
 }
 
--- billgate_studios/src/Routes/Menu.jsx
+++ billgate_studios/src/Routes/Menu.jsx
@@ -1,34 +1,95 @@
-import React from 'react'
-import { FaTwitter, FaX } from 'react-icons/fa6'
-import { NavLink } from 'react-router'
+import React from "react";
+import { FaTwitter, FaX } from "react-icons/fa6";
+import { NavLink } from "react-router";
 import { FaYoutube, FaFacebook } from "react-icons/fa";
-
+import { motion } from "motion/react";
 
 const Menu = () => {
   return (
-    <>
-        <div className='flex justify-between pl-4  mt-10 '>
-            <ul className='flex flex-col w-full ml-5'>
-                <NavLink to="/" className='text-7xl'>home</NavLink>
-                <NavLink to="/projects" className='text-7xl'>projects</NavLink>
-                <NavLink to="/categories" className='text-7xl'>categories</NavLink>
-                <NavLink to="/about" className='text-7xl'>about</NavLink>
-                <NavLink to="/contact" className='text-7xl'>contact</NavLink>
-                {/* <NavLink to="https://www.youtube.com"><FaYoutube 
-                className='mt-2 pt-3 '
-                size={30}/>Youtube</NavLink>
-                <NavLink to="https://www.twitter.com"><FaTwitter 
-                className='mt-2 pt-3 '
-                size={30}/>Twitter</NavLink>
-                <NavLink to="https://www.facebook.com"><FaFacebook
-                className='mt-2 pt-3 '
-                size={30}/>Facebook</NavLink> */}
+    <motion.div className="flex justify-between pl-4  mt-10 gap-2">
+      <section className="flex flex-col w-full ml-5 pb-5 relative overflow-hidden whitespace-nowrap">
+        <MenuLink to="/" className="text-7xl">
+         home
+        </MenuLink>
+        <MenuLink to="/projects" className="text-7xl">
+          projects
+        </MenuLink>
+        <MenuLink to="/categories" className="text-7xl">
+          categories
+        </MenuLink>
+        <MenuLink to="/about" className="text-7xl">
+          about
+        </MenuLink>
+        <MenuLink to="/contact" className="text-7xl">
+          contact
+        </MenuLink>
+      </section>
+    </motion.div>
+  );
+};
 
-            </ul>
-            <FaX className='mr-5' size={50}/>
-        </div>
-    </>
-  )
-}
+const DURATION = 0.25;
+const STAGGER = 0.025;
 
-export default Menu
+const MenuLink = ({ children, href }) => {
+  return (
+    <motion.a
+      initial="initial"
+      whileHover="hovered"
+      href={href}
+      className="relative block overflow-hidden whitespace-nowrap text-4xl font-black sm:text-7xl md:text-8xl lg:text-9xl"
+      style={{
+        lineHeight: 0.75,
+      }}
+    >
+      <div>
+        {children.split("").map((l, i) => (
+          <motion.span
+            variants={{
+              initial: {
+                y: 0,
+              },
+              hovered: {
+                y: "-100%",
+              },
+            }}
+            transition={{
+              duration: DURATION,
+              ease: "easeInOut",
+              delay: STAGGER * i,
+            }}
+            className="inline-block"
+            key={i}
+          >
+            {l}
+          </motion.span>
+        ))}
+      </div>
+      <div className="absolute inset-0">
+        {children.split("").map((l, i) => (
+          <motion.span
+            variants={{
+              initial: {
+                y: "100%",
+              },
+              hovered: {
+                y: 0,
+              },
+            }}
+            transition={{
+              duration: DURATION,
+              ease: "easeInOut",
+              delay: STAGGER * i,
+            }}
+            className="inline-block"
+            key={i}
+          >
+            {l}
+          </motion.span>
+        ))}
+      </div>
+    </motion.a>
+  );
+};
+
+export default Menu;
